c55f43f43b2ea6779e3df92ede4807ca94bff466
Nicholas Anderson <njcanderson@gmail.com>
2019-09-29 17:33:08 -0700

    Hello world to test functionality

--- background.js
+++ background.js
@@ -1,1 +1,5 @@
-  chrome.runtime.onInstalled.addListener(function() { });
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({windowLocation: 'bottom-right'}, function() {
+        console.log("The default window location is: bottom-right");
+    });
+});
